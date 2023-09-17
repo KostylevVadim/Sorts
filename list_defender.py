f8a2fb8e4bf292c53e664b8c29476d0d4892f64c
Vadim Kostylev <vadik.kostylev@yandex.ru>
2023-09-17 14:19:02 +0300

    master

---
+++ list_defender.py
@@ -0,0 +1,14 @@
+class List_Defender():
+    def __init__(self, v1):
+        self.__v = v1
+    
+    def __enter__(self):
+        self.__copy = self.__v[:]
+        return self.__copy
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.__v = self.__copy[:]
+        else:
+            print('Ошибочка')
+        return False
--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from sorters.shaker_sort import shaker_sort
 from sorters.insertions_sort import isertions_sort
 from sorters.selections_sort import selections_sort
 from sorters.merge_sort import merge_sort
-from sorters.quick_sort import quick_sort
+
 
 from decorators import Decorators, decorator
-
+from list_defender import List_Defender
 a = input().split()
-
+b = input().split()
 a = list(map(lambda x: int(x),a))
-
-@decorator
-def bs(x):
-    return bubble_sort(x)
-
-print(bs(a))
+b = list(map(lambda x: int(x),b))
+try:
+    with List_Defender(a) as a_temp:
+        for i in range(len(a_temp)):
+            a_temp[i]+=b[i]
+except Exception as e:
+    print(e)
+print(a)
 
 
 
