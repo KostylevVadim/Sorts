24224d561a6ba159ef66458b28b430979e69aa9c
Vadim Kostylev <vadik.kostylev@yandex.ru>
2023-09-17 15:36:47 +0300

    commit on master

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ from sorters.insertions_sort import isertions_sort
 from sorters.selections_sort import selections_sort
 from sorters.merge_sort import merge_sort
 
-
+import random
 from decorators import Decorators, decorator
 from list_defender import List_Defender
-a = input().split()
-b = input().split()
-a = list(map(lambda x: int(x),a))
-b = list(map(lambda x: int(x),b))
-try:
-    with List_Defender(a) as a_temp:
-        for i in range(len(a_temp)):
-            a_temp[i]+=b[i]
-except Exception as e:
-    print(e)
-print(a)
+import asyncio
+a = []
+b = []
+
+for i in range(1000000):
+    a.append(random.randint(0,1000000))
+    b.append(random.randint(0,1000000))
+x= bubble_sort(a)
+y = bubble_sort(b)
+
 
 
 
